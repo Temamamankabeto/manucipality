0e18b69aa74d7658f0e20bef5d273235a48f5088
Temam <temamaman156@gmail.com>
2026-05-15 21:39:11 -0700

    Add property management routes

--- backend/routes/property.php
+++ backend/routes/property.php
@@ -1,27 +1,27 @@
 <?php
 
- 
+use App\Http\Controllers\Api\CitizenPropertyController;
+use App\Http\Controllers\Api\PropertyCategoryController;
+use App\Http\Controllers\Api\PropertyController;
 use App\Http\Controllers\Api\UserController;
- 
 use Illuminate\Support\Facades\Route;
-
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/profile/update', [UserController::class, 'updateProfile']);
 });
+
 Route::middleware('auth:sanctum')->prefix('admin')->group(function () {
-    
 
-    //  Property Registration 
+    Route::get('/property-categories', [PropertyCategoryController::class, 'index']);
+    Route::post('/property-categories', [PropertyCategoryController::class, 'store']);
 
-    // Ownership Management 
+    Route::get('/properties', [PropertyController::class, 'index']);
+    Route::post('/properties', [PropertyController::class, 'store']);
+    Route::get('/properties/{property}', [PropertyController::class, 'show']);
+    Route::put('/properties/{property}', [PropertyController::class, 'update']);
+    Route::delete('/properties/{property}', [PropertyController::class, 'destroy']);
 
-    // Lease Management 
-
-    // Property Valuation 
-
-    // Property Search & Reporting 
-
-    
- 
+    Route::get('/citizen-properties', [CitizenPropertyController::class, 'index']);
+    Route::post('/citizen-properties', [CitizenPropertyController::class, 'store']);
+    Route::delete('/citizen-properties/{citizenProperty}', [CitizenPropertyController::class, 'destroy']);
 });
